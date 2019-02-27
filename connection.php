260af2c9d3529788da9ad73830f4c9c47de72542
yindaheng98 <yindaheng98@163.com>
2019-02-27 18:01:40 +0800

    Encodeing updated.

--- connection.php
+++ connection.php
@@ -1,10 +1,12 @@
 <?php
 $con = mysqli_connect('localhost:3306', 'Farmstay', 'Farmstay', 'Farmstay');
+mysqli_query($con,"set names utf8");
 if ($con->connect_errno)
 {
     die("连接失败: (" . $con->connect_errno . ") " . $con->connect_error);
 }
 $city = mysqli_connect('localhost:3306', 'City', 'City', 'City');
+mysqli_query($con,"set names utf8");
 if ($city->connect_errno)
 {
     die("连接失败: (" . $city->connect_errno . ") " . $city->connect_error);
