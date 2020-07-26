67a59e1e6694857d30aa9d1fdc6678687e67def1
yindaheng98 <yindaheng98@163.com>
2020-07-26 14:17:42 +0800

    添加travis支持

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
-$con = mysqli_connect('localhost:3306', 'Farmstay', 'Farmstay', 'Farmstay');
+$con = mysqli_connect('mysql:3306', 'Farmstay', 'Farmstay', 'Farmstay');
 mysqli_query($con,"set names utf8");
 if ($con->connect_errno)
 {
     die("连接失败: (" . $con->connect_errno . ") " . $con->connect_error);
 }
-$city = mysqli_connect('localhost:3306', 'City', 'City', 'City');
+$city = mysqli_connect('mysql:3306', 'City', 'City', 'City');
 mysqli_query($con,"set names utf8");
 if ($city->connect_errno)
 {
